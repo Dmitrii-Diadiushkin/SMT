49b8dd6b1bf5a3b2b32969ffc4a557fa0b76f10d
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2023-01-09 12:05:14 +0300

    Initial Commit

---
+++ CPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPTest
+//
+//  Created by Dmitrii Diadiushkin on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
